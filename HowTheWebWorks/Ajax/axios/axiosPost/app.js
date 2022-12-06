3825e11c0ebf6c4cfbb1e46ccec935992d4615b7
Nick Salvemini <salvemini.nick@gmail.com>
2022-12-05 19:36:00 -0500

    completed axios post request unit

--- HowTheWebWorks/Ajax/axios/axiosPost/app.js
+++ HowTheWebWorks/Ajax/axios/axiosPost/app.js
@@ -12,15 +12,45 @@
 
 
 
-async function getUsers() {
-    const res = await axios.get('https://hack-or-snooze.herokuapp.com/users');
+async function getUsers(token) {
+    const res = await axios.get('https://hack-or-snooze-v3.herokuapp.com/users', { params: { token } });
     console.log(res)
 }
 
 async function signUp(username, password, name) {
-    const res = await axios.post('https://hack-or-snooze.herokuapp.com/auth', { user: { name, username, password } })
+    const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/signup', { user: { name, username, password } })
     console.log(res);
 }
 
-signUp('ButtersTheChicken', 'peeword', 'Butters');
+async function logIn(username, password) {
+    const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/login', { user: { username, password } })
+    console.log(res);
+    return res.data.token;
+}
 
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkJ1dHRlcnNJc2FDaGlja2VuIiwiaWF0IjoxNjcwMjgzMTI4fQ.4n0p41P3NflHLhBLj9wvHqKvh4dW9FQTeid54FHzMZU
+
+// signUp('ButtersIsaChicken', 'peeword', 'Butters');
+
+async function getUsersWithAuth() {
+    const token = await logIn('ButtersIsaChicken', 'peeword');
+    getUsers(token);
+}
+
+async function createStory() {
+    const token = await logIn('ButtersIsaChicken', 'peeword');
+    const newStory = {
+        token,
+        story: {
+            author: 'Butters',
+            title: 'I am Butters',
+            url: "http://butters.com"
+        }
+    }
+    const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/stories', newStory);
+    console.log(res)
+}
+
+createStory()
+
+// getUsersWithAuth()
